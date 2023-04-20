56aafd8e01790c12b598ee3c6e2ff1e5500768e8
Arthur Benfica Graff <arthur_benfica@hotmail.com>
2023-04-20 18:22:06 -0300

    V2
    Sistema apresenta cadastro funcional e esta pronto para receber a listagem das despesas, esta trabalhando com LocalStorage.

---
+++ README.md
@@ -0,0 +1,5 @@
+Teste o app ↓
+
+<a href="https://benfic4rthur.github.io/App-Orcamento-Pessoal/">
+  <img src="https://conteudo.imguol.com.br/c/noticias/9b/2021/01/11/planilha-gastos-economia-financas-pessoais-contas-dinheiro-1610385192120_v2_450x337.jpg" style="width:450px; height:300px">
+</a>
---
+++ js/app.js
@@ -0,0 +1,78 @@
+class Despesa {
+  constructor(ano, mes, dia, tipo, descricao, valor) {
+    this.ano = ano;
+    this.mes = mes;
+    this.dia = dia;
+    this.tipo = tipo;
+    this.descricao = descricao;
+    this.valor = valor;
+  }
+  validarDados() {
+    for (let i in this) {
+      if (this[i] == undefined || this[i] == "" || this[i] == null) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+class Bd {
+  constructor() {
+    let id = localStorage.getItem("id");
+    if (id === null) {
+      localStorage.setItem("id", 0);
+    }
+  }
+
+  getProximoId() {
+    let proximoId = localStorage.getItem("id");
+    return proximoId == null ? 1 : parseInt(proximoId) + 1;
+  }
+  gravar(d) {
+    let id = this.getProximoId();
+    localStorage.setItem(id, JSON.stringify(d));
+    localStorage.setItem("id", id);
+  }
+}
+
+let bd = new Bd();
+
+function cadastrarDespesa() {
+  let ano = document.getElementById("ano");
+  let mes = document.getElementById("mes");
+  let dia = document.getElementById("dia");
+  let tipo = document.getElementById("tipo");
+  let descricao = document.getElementById("descricao");
+  let valor = document.getElementById("valor");
+
+  let despesa = new Despesa(
+    ano.value,
+    mes.value,
+    dia.value,
+    tipo.value,
+    descricao.value,
+    valor.value
+  );
+  if (despesa.validarDados()) {
+    bd.gravar(despesa);
+    document.getElementById("modal_titulo_div").className =
+      "modal-header text-success";
+    document.getElementById("modal_titulo").innerHTML =
+      "Registro inserido com sucesso";
+    document.getElementById("modal_conteudo").innerHTML =
+      "Despesa foi cadastrada com sucesso.";
+    document.getElementById("modal_botao").innerHTML = "Voltar";
+    document.getElementById("modal_botao").className = "btn btn-success";
+    $("#modalRegistraDespesa").modal("show");
+  } else {
+    document.getElementById("modal_titulo_div").className =
+      "modal-header text-danger";
+    document.getElementById("modal_titulo").innerHTML =
+      "Erro na inclusão do registro";
+    document.getElementById("modal_conteudo").innerHTML =
+      "Existem campos obrigatórios que não foram preenchidos.";
+    document.getElementById("modal_botao").innerHTML = "Voltar e corrigir";
+    document.getElementById("modal_botao").className = "btn btn-danger";
+    $("#modalRegistraDespesa").modal("show");
+  }
+}
